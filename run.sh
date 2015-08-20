703d63bcad69ace4efc41df48157cff3d139ced9
Loria, Mario <mloria@arbor.net>
2015-08-20 14:36:25 -0400

    we now check perms of the .ssh directory

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ STDOUT_LOC=${STDOUT_LOC:-/proc/1/fd/1}
 STDERR_LOC=${STDERR_LOC:-/proc/1/fd/2}
 
 BACKUP_DECLARATIONS=${BACKUP_DECLARATIONS:-/etc/backup.d}
-PRIV_KEY_LOC=${PRIV_KEY_LOC:-/root/.ssh/id_rsa}
+PRIV_KEY_DIR=${PRIV_KEY_DIR:-/root/.ssh}
 
-echo "0 * * * * chmod 700 ${BACKUP_DECLARATIONS}; chown -R root:root ${BACKUP_DECLARATIONS}; chmod 600 ${BACKUP_DECLARATIONS}/*; chmod 600 ${PRIV_KEY_LOC}; /usr/sbin/backupninja > ${STDOUT_LOC} 2> ${STDERR_LOC}" | crontab -
+echo "0 * * * * chmod 700 ${BACKUP_DECLARATIONS}; chown -R root:root ${BACKUP_DECLARATIONS}; chmod 600 ${BACKUP_DECLARATIONS}/*; chmod 700 ${PRIV_KEY_DIR}; chown -R root:root ${PRIV_KEY_DIR}; chmod 600 ${PRIV_KEY_DIR}/*; /usr/sbin/backupninja -d > ${STDOUT_LOC} 2> ${STDERR_LOC}" | crontab -
 exec cron -f
